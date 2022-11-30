e7b93eff4a89347e48f9879c6e3595c63f7f83ae
Jakob <jheintze33@gmail.com>
2022-11-30 11:45:49 +0100

    BERLIN WDFT JAKOB HEINTZE DONE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import "./App.css";
+import data from "./contacts.json";
+import { useState } from "react"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [contacts, setContacts] = useState(data.slice(0,5))
+  const [query, setQuery] = useState("")
+
+  function addContact(){
+    let randomContact = data[Math.floor(Math.random()*data.length)];
+    setContacts([randomContact, ...contacts])
+    console.log(contacts)
+   }
+   console.log(contacts)
+
+   function sortByPopularity() {
+    let popularitySorted = [...contacts].sort((a, b) => a.popularity < b.popularity ? -1 : 1)
+    setContacts(popularitySorted)
+  }
+
+const sortByName = () => {
+    let sortedContacts = contacts.slice().sort(function (a, b) {
+        return a.name.localeCompare(b.name)
+    })
+    setContacts(sortedContacts)
+}
+   
+function deleteContact(id) {
+  const deletedArray = [...contacts].filter(contact => {
+    return contact.id !== id
+  })
+  setContacts(deletedArray)
 }
 
+
+  return <div className="App">
+   <button onClick={addContact}>Add a Contact</button>
+   <button onClick={sortByPopularity}>Sort by popularity</button>
+            <button onClick={sortByName}>Sort by name</button>
+            
+    <table>
+      <tr>
+        <th>picture</th>
+        <th>name</th>
+        <th>popularity</th>
+        <th>Won an Oscar</th>
+        <th>Won an Emmy</th>
+      </tr>
+      {contacts.map((contact) => (
+        <tr>
+        <td> <img src={contact.pictureUrl} alt="celebPic"/> </td>
+        <th><h3>{contact.name}</h3></th>
+        <th><h4>{contact.popularity}</h4></th>
+        <th>{contact.wonOscar ? " 🏆 " : "no"}</th>
+        <th><h4>{contact.wonEmmy ? " 🏆 " : "no "}</h4></th>
+        <td><button onClick={() => deleteContact(contact.id)}>Delete</button></td>
+      </tr>
+      ))}
+    </table>
+  </div>;
+}
 export default App;
